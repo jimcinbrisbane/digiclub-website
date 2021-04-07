4c669ec59a8d99bc1bc43575df33ba44b9d83ade
natsumadan <madan.mohit209@gmail.com>
2021-04-07 14:14:04 +1000

    update

---
+++ digiclub-website-react/src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+    state = {
+        headerText: 'This is DigiClubs Website!!',
+    };
+    render (){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
