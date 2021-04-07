67de0b3bdaa9e0a609f013da436a2e4978a025bc
natsumadan <madan.mohit209@gmail.com>
2021-04-07 14:25:44 +1000

    update

---
+++ digiclub-website-react/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css'
+
+
+const Header = ({}) => {
+    return(
+        <div className="heade-container">
+            <img
+            src= ""
+            className={`head-image`}
+            alt="headerImage"/>
+            <hi className={`head-text`}>
+
+            </hi>
+        </div>
+    );
+};
+
+
+
+export default Header;
