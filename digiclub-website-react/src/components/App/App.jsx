33e5f31a10c69681fbb806300930d4e1acf23088
natsumadan <madan.mohit209@gmail.com>
2021-04-07 14:29:14 +1000

    Header

--- digiclub-website-react/src/components/App/App.jsx
+++ digiclub-website-react/src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from '../Header/Header';
 import './App.css';
 
 
@@ -9,6 +10,7 @@ class App extends React.Component {
     render (){
         return (
             <div>
+                <Header/>
                 
             </div>
         )
